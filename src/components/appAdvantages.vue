15ba9b24dd1df003cc614eb65d1d2916179ccf8b
Vahe <w33bv.gl@gmail.com>
2024-01-20 11:52:27 +0400

    update advantages

--- src/components/appAdvantages.vue
+++ src/components/appAdvantages.vue
@@ -1,5 +1,171 @@
 <template>
-  <section></section>
+  <section class="advantages">
+    <div class="advantages-inner">
+      <div class="adv-title">
+        <div class="adv">
+          <div class="adv-inner">
+            <div class="adv-content">
+              <div class="advesting-child">
+                <div class="adv-img">
+                  <img
+                    src="../../public/assets/img/advertising/shipped.png"
+                    alt=""
+                  />
+                </div>
+                <div class="adv-inner-contnent">
+                  <h4>ԱՆՎՃԱՐ ԱՌԱՔՈՒՄ</h4>
+                  <p>1000 ֏-ից բարձր պատվերների դեպքում</p>
+                </div>
+              </div>
+            </div>
+            <div class="adv-content">
+              <div class="advesting-child">
+                <div class="adv-img">
+                  <img
+                    src="../../public/assets/img/advertising/credit-card.png"
+                    alt=""
+                  />
+                </div>
+                <div class="adv-inner-contnent">
+                  <h4>ԱՆՎՏԱՆԳ ՎՃԱՐՈՒՄՆԵՐ</h4>
+                  <p>100% ապահով վճարում</p>
+                </div>
+              </div>
+            </div>
+            <div class="adv-content">
+              <div class="advesting-child">
+                <div class="adv-img">
+                  <img
+                    src="../../public/assets/img/advertising/chat.png"
+                    alt=""
+                  />
+                </div>
+                <div class="adv-inner-contnent">
+                  <h4>ՕԳՆՈՒԹՅՈՒՆ 24/7</h4>
+                  <p>24/7 աջակցություն</p>
+                </div>
+              </div>
+            </div>
+            <div class="adv-content">
+              <div class="advesting-child">
+                <div class="adv-img">
+                  <img
+                    src="../../public/assets/img/advertising/money.png"
+                    alt=""
+                  />
+                </div>
+                <div class="adv-inner-contnent">
+                  <h4>Կվերադարձնենք</h4>
+                  <p>Եթե ապրանքները խնդիրներ ունեն</p>
+                </div>
+              </div>
+            </div>
+            <div class="adv-content">
+              <div class="advesting-child">
+                <div class="adv-img">
+                  <img
+                    src="../../public/assets/img/advertising/id-card.png"
+                    alt=""
+                  />
+                </div>
+                <div class="adv-inner-contnent">
+                  <h4>ԾԱՌԱՅՈՒԹՅՈՒՆՆԵՐ</h4>
+                  <p>30 օր երաշխիք</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
+
 <script></script>
-<style scoped lang="scss"></style>
+
+<style scoped lang="scss">
+.advantages {
+  background-color: var(--advantages-background-color);
+  width: 100%;
+  padding-top: 0;
+  padding-bottom: 40px;
+
+  .advantages-inner {
+    width: 100%;
+    padding: 0 10px;
+
+    .adv-title {
+      max-width: 1800px;
+      margin: 0 auto;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      gap: 20px;
+
+      .adv {
+        background-color: var(--adv-background-color);
+        width: 100%;
+
+        .adv-inner {
+          width: 100%;
+          padding: 10px 0;
+          display: flex;
+          flex-wrap: wrap;
+
+          .adv-content {
+            width: 20%;
+            height: 50px;
+            margin: 10px 0;
+            border-right: 1px solid var(--adv-content-border-color);
+            display: flex;
+
+            .advesting-child {
+              height: 100%;
+              padding: 0 10px;
+              margin: 0 auto;
+              display: flex;
+              align-items: center;
+
+              .adv-img {
+                height: 100%;
+                display: flex;
+                align-items: center;
+                margin-right: 20px;
+                pointer-events: none;
+
+                img {
+                  width: 50px;
+                  pointer-events: none;
+                }
+              }
+
+              .adv-inner-contnent {
+                width: 100%;
+                height: 100%;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+
+                h4 {
+                  margin-bottom: 4px;
+                  font-size: 13px;
+                  text-transform: uppercase;
+                  font-weight: bold;
+                }
+
+                p {
+                  color: var(--adv-title-p-color);
+                }
+              }
+            }
+          }
+
+          .adv-content:last-child {
+            border: none; // for last item
+          }
+        }
+      }
+    }
+  }
+}
+</style>
